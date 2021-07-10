45e9f804cb77fe8acc88fc5923f98b3e529abeef
Gabriel Wolf <gabrielfai70@gmail.com>
2021-07-10 09:23:20 -0300

    Versão 1.0

---
+++ phpmvclp3/MVC/Model/Conexao.php
@@ -0,0 +1,27 @@
+<?php
+class Conexao
+{
+    public static function getConexao()
+    {
+        // CONFIGURAÇÕES DE SERVIDOR E BANCO DE DADOS
+        $servername = ""; 
+        $username = "";
+        $password = "";
+        $dbname = "";
+
+        try 
+        {
+           $minhaConexao = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+           // set the PDO error mode to exception
+           $minhaConexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           return $minhaConexao;
+        }
+        catch(PDOException $e) 
+        {
+         echo "entrou no catch".$e->getmessage();
+         return null;
+       }
+    }
+
+}
+?>
